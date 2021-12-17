db10e5239a88af6e6d6bb9e26ff7b424966e4039
Alexandra <iamcazacu@gmail.com>
2021-12-17 02:26:42 +0200

    First commit

--- src/main/java/com/github/dawidstankiewicz/forum/user/User.java
+++ src/main/java/com/github/dawidstankiewicz/forum/user/User.java
@@ -2,16 +2,8 @@ package com.github.dawidstankiewicz.forum.user;
 
 import java.io.Serializable;
 import java.util.Date;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.PrePersist;
-import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.*;
+
 import lombok.Data;
 
 @Entity
